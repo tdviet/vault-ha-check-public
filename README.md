5c800acf786b095912673a74817dd8db36c082fb
tdviet <tdvet@gmail.com>
2022-07-07 20:23:14 +0200

    Update commit

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 This is the public version of Python script for Vault HA
 
 The script check generic endpoint, and if something is wrong, 
-check service instances and update the the generic endpoint to 
+check service instances and update the generic endpoint to 
 the healthy instance. Raise alarm if no healthy instance found
--- vault-ha-check.py
+++ vault-ha-check.py
@@ -7,7 +7,7 @@ import requests
 import socket
 
 GENERIC_HOSTNAME = "vault.services.fedcloud.eu"
-UPDATE_SECRET = "XXXXXX"
+UPDATE_SECRET = "XXXXX"
 INSTANCE_HOSTNAMES = ("vault-infn.services.fedcloud.eu", "vault-ifca.services.fedcloud.eu")
 
 
@@ -21,11 +21,11 @@ def check_server_health(server):
     try:
         r = requests.head(f"https://{server}:8200/v1/sys/health", timeout=4)
         status = r.status_code
-        if status in (200, 409):
-            print(f"Server {server} is OK", file=sys.stderr)
+        if status in (200, 429):
+            print(f"Server {server} is OK")
             return True
     except Exception as e:
-        print(f'[ERROR]: {e}', file=sys.stderr)
+        print(f'[ERROR]: {e}')
 
     # Return False by default
     return False
@@ -41,7 +41,7 @@ def update_generic_endpoint(generic, server, update_secret):
     """
     ip = socket.gethostbyname(server)
     update_string = f"https://{generic}:{update_secret}@nsupdate.fedcloud.eu/nic/update?hostname={generic}&myip={ip}"
-    print(f"Update string: {update_string}", file=sys.stderr)
+    # print(f"Update string: {update_string}", file=sys.stderr)
     r = requests.get(update_string, timeout=4)
     print(f"Server response: {r.text}", file=sys.stderr)
 
@@ -55,15 +55,28 @@ def check_and_update(generic, instances, update_secret):
     :return: 0 if OK, 2 for Critical
     """
 
+    # First, check the health of generic endpoint
     if check_server_health(generic):
-        print(f"Generic server {generic} is OK, nothing to do", file=sys.stderr)
+
+        # If OK, nothing to do, print OK message and return OK
+        print(f"Generic server {generic} is OK, nothing to do")
         return 0
+
     else:
+        # Instance at generic endpoint is faulty
+
+        # Looking for a healthy instance among the instance list
         for instance in instances:
             if check_server_health(instance):
-                print(f"Update {generic} to instance {instance}", file=sys.stderr)
+
+                # Found a healthy one, updating generic endpoint to it
+                print(f"Update {generic} to instance {instance}")
                 update_generic_endpoint(generic, instance, update_secret)
+
+                # Generic endpoint updated, return OK
                 return 0
+
+        # No healthy instance found, print error message and return CRITICAL
         print("Error: All servers are faulty. Raise alarm !!!")
         return 2
 
